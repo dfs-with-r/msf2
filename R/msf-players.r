961d5d895aed9b36c0f0339bfe573aa0ce7a5508
Robert Zamora <zamora.rr@gmail.com>
2018-07-20 09:14:27 -0400

    all_players

---
+++ R/msf-players.r
@@ -0,0 +1,19 @@
+#' Lists all players for a league on a given date, with full bio and other details.
+#' @param date given date
+#' @param ... optional query parameters
+#'
+#' @examples
+#' \dontrun{
+#' j <- all_players("mlb", team = "bos", rosterstatus = "assigned-to-roster,assigned-to-injury-list")
+#' }
+all_players <- function(sport, date = Sys.Date(), ...) {
+  stopifnot(length(date) == 1L, length(sport) == 1L)
+  path <- sprintf("%s/players.json", sport)
+
+  query <- list(date  = msf_date(date), ...)
+
+  result <- msf_api(path, query)
+  attr(result, "local_path") <- sprintf("%s/players-%s.json", sport, format(date, "%Y%m%d"))
+
+  result
+}
